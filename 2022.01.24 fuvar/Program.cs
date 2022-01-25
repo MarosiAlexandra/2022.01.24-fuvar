5ce9bf176f1350a7259537966dd7f17a79336f86
MarosiAlexandra <97289704+MarosiAlexandra@users.noreply.github.com>
2022-01-25 13:16:58 +0100

    3

--- 2022.01.24 fuvar/Program.cs
+++ 2022.01.24 fuvar/Program.cs
@@ -11,6 +11,14 @@ namespace _2022._01._24_fuvar
     {
         static void Main(string[] args)
         {
+            List<Fuvar> fuvarok = new List<Fuvar>();
+            foreach ( sor in File.ReadAllLines("fuvar.css").Skip(1))
+
+            {
+                fuvarok.Add(new Fuvar(sor));
+            }
+
+
             Console.ReadKey();
         }
     }
